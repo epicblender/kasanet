ad7fccb474a9889336354b3eae71ccee4f2f14f5
blender <64718307+epicblender@users.noreply.github.com>
2025-11-22 00:36:54 +1100

    made teto squashable

--- scripts/script.js
+++ scripts/script.js
@@ -10,11 +10,14 @@ img.addEventListener('mouseenter', () => {
 const image = document.querySelector('.wide-image');
 const wrapper = document.querySelector('.wide-wrapper');
 
+const initialHeight = image.offsetHeight;
+const initialWidth = image.offsetWidth;
+
 // bottomLimit must be > animationOffset + topLimit
 window.addEventListener('scroll', () => {
   const animationOffset = window.innerHeight * 0.5;
   const topLimit = wrapper.offsetTop;
-  const bottomLimit = topLimit + wrapper.offsetHeight - (image.offsetHeight / 2 + window.innerHeight / 2);
+  const bottomLimit = topLimit + wrapper.offsetHeight - (initialHeight / 2 + window.innerHeight / 2);
   
   const scroll = window.scrollY;
 
@@ -23,20 +26,35 @@ window.addEventListener('scroll', () => {
     image.style.top = '50vh';
     image.style.left = '75vw';
     image.style.opacity = '0';
+    image.style.maxHeight = initialHeight + 'px';
+    image.width = initialWidth;
   } else if (scroll < topLimit + animationOffset) {
     image.style.position = 'fixed';
     image.style.top = '50vh';
     image.style.left = '75vw';
     image.style.opacity = '0';
-  } else if (scroll < bottomLimit) {
+    image.style.maxHeight = initialHeight + 'px';
+    image.width = initialWidth;
+  }else if (scroll < bottomLimit) {
     image.style.position = 'fixed';
     image.style.top = '50vh';
     image.style.left = '50vw';
     image.style.opacity = '1';
+    image.style.maxHeight = initialHeight + 'px';
+    image.width = initialWidth;
+  } else if (scroll < bottomLimit - initialHeight) {
+    image.style.position = 'fixed';
+    image.style.top = '50vh';
+    image.style.left = '50vw';
+    image.style.opacity = '1';
+    image.style.maxHeight = topLimit + wrapper.offsetHeight - scroll + 'px';
+    image.width = initialWidth;
   } else {
     image.style.position = 'absolute';
     image.style.top = (wrapper.offsetHeight - image.offsetHeight / 2) + 'px';
     image.style.left = '50vw';
     image.style.opacity = '1';
+    image.style.maxHeight = topLimit + wrapper.offsetHeight - scroll + 'px';
+    image.width = initialWidth;
   }
 });
