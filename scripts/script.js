dd38ad6562e18022a83ab3dc5ef1ee49488fdb3b
blender <64718307+epicblender@users.noreply.github.com>
2025-11-21 21:44:53 +1100

    added manually sticky scrollable teto

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,25 @@ img.addEventListener('mouseenter', () => {
   sound.currentTime = 0;
   sound.play();
 });
+
+const image = document.querySelector('.wide-image');
+const wrapper = document.querySelector('.wide-wrapper');
+
+window.addEventListener('scroll', () => {
+  const wrapperStart = wrapper.getBoundingClientRect().top + window.scrollY;
+
+  const topLimit = wrapperStart;
+  const bottomLimit = wrapperStart + window.innerHeight;
+  const scroll = window.scrollY;
+
+  if (scroll >= topLimit && scroll <= bottomLimit) {
+    image.style.position = 'fixed';
+    image.style.top = '0';
+  } else if (scroll < topLimit) {
+    image.style.position = 'absolute';
+    image.style.top = '0';
+  } else {
+    image.style.position = 'absolute';
+    image.style.top = (wrapper.offsetHeight - image.offsetHeight) + 'px';
+  }
+});
