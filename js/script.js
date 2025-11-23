dd8e6b5b26c7241073cf948dafbd207b34b77e60
blender <64718307+epicblender@users.noreply.github.com>
2025-11-23 15:11:05 +1100

    fixed minor style

--- js/script.js
+++ js/script.js
@@ -11,7 +11,10 @@ const image = document.querySelector("#wide-image");
 const wrapper = document.querySelector("#wide-wrapper");
 
 const initialHeight = image.offsetHeight;
-const initialWidth = image.offsetWidth;
+
+function changeStyle(position, top, left, opacity, maxHeight) {
+  Object.assign(image.style, { position, top, left, opacity, maxHeight });
+}
 
 // bottomLimit must be > animationOffset + topLimit
 window.addEventListener("scroll", () => {
@@ -25,40 +28,27 @@ window.addEventListener("scroll", () => {
   const scroll = window.scrollY;
 
   if (scroll < topLimit) {
-    image.style.position = "absolute";
-    image.style.top = "50vh";
-    image.style.left = "75vw";
-    image.style.opacity = "0";
-    image.style.maxHeight = initialHeight + "px";
-    image.width = initialWidth;
+    changeStyle("absolute", "50vh", "75vw", "0", initialHeight + "px");
   } else if (scroll < topLimit + animationOffset) {
-    image.style.position = "fixed";
-    image.style.top = "50vh";
-    image.style.left = "75vw";
-    image.style.opacity = "0";
-    image.style.maxHeight = initialHeight + "px";
-    image.width = initialWidth;
+    changeStyle("fixed", "50vh", "75vw", "0", initialHeight + "px");
   } else if (scroll < bottomLimit) {
-    image.style.position = "fixed";
-    image.style.top = "50vh";
-    image.style.left = "50vw";
-    image.style.opacity = "1";
-    image.style.maxHeight = initialHeight + "px";
-    image.width = initialWidth;
+    changeStyle("fixed", "50vh", "50vw", "1", initialHeight + "px");
   } else if (scroll < bottomLimit - initialHeight) {
-    image.style.position = "fixed";
-    image.style.top = "50vh";
-    image.style.left = "50vw";
-    image.style.opacity = "1";
-    image.style.maxHeight = topLimit + wrapper.offsetHeight - scroll + "px";
-    image.width = initialWidth;
+    changeStyle(
+      "fixed",
+      "50vh",
+      "50vw",
+      "1",
+      topLimit + wrapper.offsetHeight - scroll + "px"
+    );
   } else {
-    image.style.position = "absolute";
-    image.style.top = wrapper.offsetHeight - image.offsetHeight / 2 + "px";
-    image.style.left = "50vw";
-    image.style.opacity = "1";
-    image.style.maxHeight = topLimit + wrapper.offsetHeight - scroll + "px";
-    image.width = initialWidth;
+    changeStyle(
+      "absolute",
+      wrapper.offsetHeight - image.offsetHeight / 2 + "px",
+      "50vw",
+      "1",
+      topLimit + wrapper.offsetHeight - scroll + "px"
+    );
   }
 });
 
@@ -72,170 +62,3 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 document.querySelectorAll(".fade-in").forEach((el) => observer.observe(el));
-
-gsap.registerPlugin(MorphSVGPlugin);
-
-const tl = gsap.timeline();
-const delay = 0.4;
-const fill = "#FF5C5C";
-
-tl.to("#preload-box-1 path", {
-  duration: 0,
-  fill,
-});
-
-tl.to("#preload-box-4 path", {
-  duration: 0,
-  fill,
-  delay,
-});
-
-tl.to("#preload-box-2 path", {
-  duration: 0,
-  fill,
-  delay,
-});
-
-tl.to("#preload-box-3 path", {
-  duration: 0,
-  fill,
-  delay,
-});
-
-tl.to("#left-drill", {
-  duration: 0.5,
-  morphSVG: "M0,0 H200 V40 H120 V200 H100 H80 V40 H0 Z",
-  ease: "power2.inOut",
-  delay,
-});
-
-tl.to(
-  "#right-drill",
-  {
-    duration: 0.5,
-    morphSVG: "M0,0 H200 V40 H120 V200 H100 H80 V40 H0 Z",
-    ease: "power2.inOut",
-  },
-  "<"
-);
-
-tl.to(
-  "#right-eye",
-  {
-    duration: 0.5,
-    morphSVG:
-      "M100,0 A100,100 0 1,1 99.999,0 Z M100,40 A60,60 0 1,1 99.999,40 Z",
-    ease: "power2.inOut",
-  },
-  "<"
-);
-
-const theta = Math.PI / 8;
-const x1 = 100 * Math.cos(theta);
-const x2 = 60 * Math.cos(theta);
-const y1 = 100 * Math.sin(theta);
-const y2 = 60 * Math.sin(theta);
-
-tl.to(
-  "#left-eye",
-  {
-    duration: 0.5,
-    morphSVG: `
-    M${100 + x1},${100 - y1} A100,100 0 1,0 ${100 + x1},${100 + y1}
-    L${100 + x2},${100 + y2} A60,60 0 1,1 ${100 + x2},${100 - y2} Z
-  `,
-    ease: "power2.inOut",
-  },
-  "<"
-);
-
-tl.to("#preload-title", {
-  duration: 0.5,
-  gap: "1vw",
-  ease: "power2.inOut",
-});
-
-tl.set("#preload-box-1", { order: 1 })
-  .set("#preload-box-2", { order: 2 })
-  .set("#preload-box-3", { order: 4 })
-  .set("#preload-box-4", { order: 3 })
-  .to({}, { duration: 0 });
-
-tl.to(
-  "#preload-title svg",
-  {
-    scale: 1.1,
-    duration: 0,
-  },
-  "<"
-);
-
-tl.to(
-  "#preload-title svg path",
-  {
-    duration: 0,
-    fill: "#ffc2c2",
-  },
-  "<"
-);
-
-tl.to(
-  "#preload-title svg",
-  {
-    scale: 1,
-    duration: 1,
-    ease: "power1.inOut",
-  },
-  "<"
-);
-
-tl.to(
-  "#preload-title svg path",
-  {
-    duration: 1.5,
-    fill,
-  },
-  "<"
-);
-
-tl.to(
-  "#preload-bg-cover",
-  {
-    duration: 1.5,
-    y: "100vh",
-    ease: "power2.inOut",
-  },
-  "<"
-);
-
-tl.to(
-  "#preload-bg-cover",
-  {
-    duration: 1.5,
-    y: "100vh",
-    ease: "power2.inOut",
-  },
-  "<"
-);
-
-tl.to(
-  "#preload-title",
-  {
-    duration: 1,
-    y: "-50px",
-    ease: "power2.inOut",
-    delay: 0.7,
-  },
-  "<"
-);
-
-tl.to(
-  "#preload-subtitle",
-  {
-    duration: 1,
-    y: "30px",
-    opacity: 1,
-    ease: "power2.inOut",
-  },
-  "<"
-);
